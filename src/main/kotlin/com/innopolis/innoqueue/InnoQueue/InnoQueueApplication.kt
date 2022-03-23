be1624fb1ae3a4b71a509183cfbf1719a5683279
SMore-Napi <s.roma245@gmail.com>
2022-03-23 13:18:28 +0300

    Initial project

---
+++ src/main/kotlin/com/innopolis/innoqueue/InnoQueue/InnoQueueApplication.kt
@@ -0,0 +1,11 @@
+package com.innopolis.innoqueue.InnoQueue
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InnoQueueApplication
+
+fun main(args: Array<String>) {
+	runApplication<InnoQueueApplication>(*args)
+}
---
+++ src/test/kotlin/com/innopolis/innoqueue/InnoQueue/InnoQueueApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.innopolis.innoqueue.InnoQueue
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InnoQueueApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
