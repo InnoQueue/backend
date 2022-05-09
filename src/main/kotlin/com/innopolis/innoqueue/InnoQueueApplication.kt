f37081d450c60d2653bc070cf62d41e7e3ed03cd
SMore-Napi <s.roma245@gmail.com>
2022-05-09 15:04:53 +0300

    Reset DB every hour

--- src/main/kotlin/com/innopolis/innoqueue/InnoQueueApplication.kt
+++ src/main/kotlin/com/innopolis/innoqueue/InnoQueueApplication.kt
@@ -12,11 +12,37 @@ class InnoQueueApplication
 
 fun main(args: Array<String>) {
     val startupDelay: Long = 1 * 60 * 1000
+    val resetDbTimeMinutes: Long = 60
+    val resetDbTimeMillis: Long = resetDbTimeMinutes * 60 * 1000
     val clearInviteCodesTimeMinutes: Long = 30
     val clearInviteCodesTimeMillis: Long = clearInviteCodesTimeMinutes * 60 * 1000
     val clearNotificationsTimeHours: Long = 24
     val clearNotificationsTimeMillis: Long = clearNotificationsTimeHours * 60 * 60 * 1000
     val context = runApplication<InnoQueueApplication>(*args)
+    thread(start = true) {
+        Thread.sleep(startupDelay)
+        while (true) {
+            val db = context.getBean("databaseController") as DatabaseController
+            try {
+                db.resetDB()
+            } catch (_: Exception) {
+            }
+            println("Database was reset")
+            Thread.sleep(resetDbTimeMillis)
+        }
+    }
+    thread(start = true) {
+        Thread.sleep(startupDelay)
+        while (true) {
+            val db = context.getBean("databaseController") as DatabaseController
+            try {
+                db.clearCodes()
+            } catch (_: Exception) {
+            }
+            println("Expired invite codes were deleted")
+            Thread.sleep(clearInviteCodesTimeMillis)
+        }
+    }
     thread(start = true) {
         Thread.sleep(startupDelay)
         while (true) {
