fe322c4139296ae2d7661232ead6c5d0f08acea2
SMore-Napi <s.roma245@gmail.com>
2022-05-09 14:56:40 +0300

    Database default mock data

--- restore.sql
+++ restore.sql
@@ -196,22 +196,20 @@ CREATE SEQUENCE queue_qr_code2_id_seq START WITH 100 INCREMENT BY 1;
 
 
 INSERT INTO public."user" (user_id, token, name, fcm_token)
-VALUES (4, '4', 'Peter', '3');
-INSERT INTO public."user" (user_id, token, name, fcm_token)
-VALUES (3, '3', 'Bob', '5');
-INSERT INTO public."user" (user_id, token, name, fcm_token)
-VALUES (5, '5', 'Alice', '6');
-INSERT INTO public."user" (user_id, token, name, fcm_token)
-VALUES (15, '22222', 'Emil', '22222');
-INSERT INTO public."user" (user_id, token, name, fcm_token)
-VALUES (2, '2', 'Ivan', '2');
-INSERT INTO public."user" (user_id, token, name, fcm_token)
 VALUES (1, '11111', 'admin', '11111');
+INSERT INTO public."user" (user_id, token, name, fcm_token)
+VALUES (2, '22222', 'Emil', '22222');
+INSERT INTO public."user" (user_id, token, name, fcm_token)
+VALUES (3, '33333', 'Roman', '33333');
+INSERT INTO public."user" (user_id, token, name, fcm_token)
+VALUES (4, '44444', 'Timur', '44444');
+INSERT INTO public."user" (user_id, token, name, fcm_token)
+VALUES (5, '55555', 'Ivan', '55555');
 
 
 INSERT INTO public.user_settings (user_settings_id, user_id, completed, skipped, joined_queue, "freeze", left_queue,
                                   your_turn)
-VALUES (10, 15, true, true, true, true, true, true);
+VALUES (1, 1, true, true, true, true, true, true);
 INSERT INTO public.user_settings (user_settings_id, user_id, completed, skipped, joined_queue, "freeze", left_queue,
                                   your_turn)
 VALUES (2, 2, true, true, true, true, true, true);
@@ -224,120 +222,93 @@ VALUES (4, 4, true, true, true, true, true, true);
 INSERT INTO public.user_settings (user_settings_id, user_id, completed, skipped, joined_queue, "freeze", left_queue,
                                   your_turn)
 VALUES (5, 5, true, true, true, true, true, true);
-INSERT INTO public.user_settings (user_settings_id, user_id, completed, skipped, joined_queue, "freeze", left_queue,
-                                  your_turn)
-VALUES (1, 1, true, true, true, true, true, true);
 
 
 INSERT INTO public.queue (queue_id, name, color, creator_id, track_expenses, current_user_id)
-VALUES (44, 'Bring Water', 'BLUE', 1, false, 1);
+VALUES (6, 'Buy Toilet Paper', 'RED', 2, true, 2);
+INSERT INTO public.queue (queue_id, name, color, creator_id, track_expenses, current_user_id)
+VALUES (34, 'Buy Dishwashing Soap', 'GREEN', 1, true, 3);
+INSERT INTO public.queue (queue_id, name, color, creator_id, track_expenses, current_user_id)
+VALUES (39, 'Trash', 'YELLOW', 2, false, 1);
 INSERT INTO public.queue (queue_id, name, color, creator_id, track_expenses, current_user_id)
 VALUES (40, 'Buy Soap', 'ORANGE', 1, true, 1);
 INSERT INTO public.queue (queue_id, name, color, creator_id, track_expenses, current_user_id)
-VALUES (46, 'Buy Sponge', 'PURPLE', 1, true, 15);
+VALUES (44, 'Bring Water', 'BLUE', 1, false, 1);
 INSERT INTO public.queue (queue_id, name, color, creator_id, track_expenses, current_user_id)
-VALUES (6, 'Buy Toilet Paper', 'RED', 15, true, 15);
-INSERT INTO public.queue (queue_id, name, color, creator_id, track_expenses, current_user_id)
-VALUES (39, 'Trash', 'YELLOW', 15, false, 1);
-INSERT INTO public.queue (queue_id, name, color, creator_id, track_expenses, current_user_id)
-VALUES (34, 'Buy Dishwashing Soap', 'GREEN', 1, true, 15);
+VALUES (46, 'Buy Sponge', 'PURPLE', 1, true, 4);
 
 
 INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
 VALUES (1, 1, 'JOINED_QUEUE', 5, 44, true, current_timestamp + INTERVAL '- 10 DAY');
 INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (2, 15, 'JOINED_QUEUE', 5, 44, true, current_timestamp + INTERVAL '- 10 DAY 1 hour');
+VALUES (2, 1, 'SKIPPED', 3, 40, true, current_timestamp + INTERVAL '- 9 DAY');
 INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (3, 1, 'SKIPPED', 3, 40, true, current_timestamp + INTERVAL '- 9 DAY');
+VALUES (3, 1, 'COMPLETED', 2, 44, true, current_timestamp + INTERVAL '- 8 DAY');
 INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (4, 15, 'SKIPPED', 3, 40, true, current_timestamp + INTERVAL '- 9 DAY 1 hour');
+VALUES (4, 1, 'COMPLETED', 5, 39, false, current_timestamp + INTERVAL '- 7 DAY');
 INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (5, 1, 'COMPLETED', 2, 44, true, current_timestamp + INTERVAL '- 8 DAY');
+VALUES (5, 1, 'YOUR_TURN', 1, 39, false, current_timestamp + INTERVAL '- 7 DAY 2 hour');
 INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (6, 15, 'COMPLETED', 2, 44, true, current_timestamp + INTERVAL '- 8 DAY 1 hour');
-INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (7, 1, 'COMPLETED', 5, 39, false, current_timestamp + INTERVAL '- 7 DAY');
-INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (8, 15, 'COMPLETED', 5, 39, false, current_timestamp + INTERVAL '- 7 DAY 1 hour');
-INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (9, 1, 'YOUR_TURN', 1, 39, false, current_timestamp + INTERVAL '- 7 DAY 2 hour');
-INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (10, 15, 'YOUR_TURN', 15, 39, false, current_timestamp + INTERVAL '- 7 DAY 3 hour');
-INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (11, 15, 'SHOOK', 15, 39, false, current_timestamp + INTERVAL '- 1 DAY');
-INSERT INTO public.notifications (notification_id, user_id, message_type, participant_id, queue_id, is_read, date)
-VALUES (12, 1, 'SHOOK', 1, 39, false, current_timestamp + INTERVAL '- 1 DAY 1 hour');
+VALUES (6, 1, 'SHOOK', 1, 39, false, current_timestamp + INTERVAL '- 1 DAY 1 hour');
 
 
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (19, 44, 15, false, 0, 0, false, '2022-04-30 20:11:05.312758');
+VALUES (1, 44, 1, true, 0, 0, false, '2022-05-09 10:51:18.312000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (23, 39, 15, true, 0, 0, false, '2022-04-30 20:11:48.209418');
+VALUES (28, 46, 5, false, 0, 50.6, false, '2022-05-09 11:07:59.745000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (60, 39, 2, true, 0, 0, false, '2022-03-26 15:37:27.000000');
+VALUES (6, 39, 2, true, 0, 0, false, '2022-05-09 10:57:19.938000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (67, 44, 1, true, 0, 0, false, '2022-03-26 12:46:48.649000');
+VALUES (18, 39, 3, true, 0, 0, false, '2022-05-09 11:05:21.981000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (81, 39, 3, true, 0, 0, false, '2022-03-26 16:05:04.000000');
+VALUES (13, 44, 3, true, 0, 0, false, '2022-05-09 11:04:36.458000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (82, 39, 4, true, 0, 0, false, '2022-03-26 16:05:06.000000');
+VALUES (19, 44, 4, true, 0, 0, false, '2022-05-09 11:06:00.226000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (83, 39, 5, true, 0, 0, false, '2022-03-26 16:05:07.000000');
+VALUES (7, 44, 2, true, 0, 0, false, '2022-05-09 11:01:44.692000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (87, 44, 2, true, 0, 0, false, '2022-03-26 16:06:40.000000');
+VALUES (24, 39, 4, true, 0, 0, false, '2022-05-09 11:06:39.121000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (90, 44, 5, true, 0, 0, false, '2022-03-26 16:06:43.000000');
+VALUES (30, 39, 5, true, 0, 0, false, '2022-05-09 11:08:14.344000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (68, 39, 1, true, 0, 0, true, '2022-03-26 15:58:10.000000');
+VALUES (12, 39, 1, true, 0, 0, true, '2022-05-09 11:03:41.915000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (89, 44, 4, false, 0, 0, false, '2022-03-26 16:06:42.000000');
+VALUES (4, 34, 1, false, 0, 50, false, '2022-05-09 10:52:22.417000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (88, 44, 3, false, 0, 0, false, '2022-03-26 16:06:41.000000');
+VALUES (3, 46, 1, false, 0, 45, false, '2022-05-09 10:51:58.522000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (25, 46, 15, true, 0, 40, false, '2022-04-30 20:12:03.499760');
+VALUES (25, 44, 5, false, 0, 0, false, '2022-05-09 11:07:28.740000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (69, 40, 1, true, 0, 92, false, '2022-03-26 15:58:31.000000');
+VALUES (8, 34, 2, true, 0, 120.87, false, '2022-05-09 11:01:54.188000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (54, 34, 1, false, 0, 200, true, '2022-03-26 12:05:51.988000');
+VALUES (10, 46, 2, true, 0, 47.59, false, '2022-05-09 11:02:14.258000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (73, 6, 5, true, 0, 120, false, '2022-03-26 16:03:31.000000');
+VALUES (16, 46, 3, true, 0, 41.7, false, '2022-05-09 11:05:03.947000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (72, 6, 4, true, 0, 90, false, '2022-03-26 16:03:29.000000');
+VALUES (17, 6, 3, true, 0, 198.4, false, '2022-05-09 11:05:12.865000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (74, 34, 2, true, 0, 210, false, '2022-03-26 16:04:19.000000');
+VALUES (5, 6, 2, true, 0, 219.89, false, '2022-05-09 10:57:02.583000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (22, 6, 15, true, 0, 123, false, '2022-04-30 20:11:39.530443');
+VALUES (23, 6, 4, true, 0, 187.68, false, '2022-05-09 11:06:31.234000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (71, 6, 3, true, 0, 100, false, '2022-03-26 16:03:28.000000');
+VALUES (11, 6, 1, true, 0, 200, false, '2022-05-09 11:03:33.380000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (62, 40, 2, true, 0, 82, false, '2022-03-26 15:37:49.000000');
+VALUES (29, 6, 5, true, 0, 224.7, false, '2022-05-09 11:08:07.399000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (24, 34, 15, true, 0, 248, false, '2022-04-30 20:11:55.648157');
+VALUES (14, 34, 3, true, 0, 234.6, false, '2022-05-09 11:04:44.603000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (77, 34, 5, true, 0, 190.87, false, '2022-03-26 16:04:23.000000');
+VALUES (26, 34, 5, false, 0, 321.87, false, '2022-05-09 11:07:39.780000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (93, 46, 4, true, 0, 50.28, false, '2022-03-26 16:07:34.000000');
+VALUES (20, 34, 4, true, 0, 123.45, false, '2022-05-09 11:06:07.441000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (86, 40, 5, true, 0, 90.87, false, '2022-03-26 16:06:01.000000');
+VALUES (27, 40, 5, true, 0, 123.45, false, '2022-05-09 11:07:49.280000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (10, 6, 1, true, 0, 140.16, false, '2022-03-02 22:36:56.000000');
+VALUES (15, 40, 3, true, 0, 100, false, '2022-05-09 11:04:53.193000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (21, 6, 2, true, 0, 117.86, false, '2022-03-25 01:32:42.000000');
+VALUES (21, 40, 4, true, 0, 90, false, '2022-05-09 11:06:16.359000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (75, 34, 3, true, 0, 201.04, false, '2022-03-26 16:04:21.000000');
+VALUES (9, 40, 2, true, 0, 120, false, '2022-05-09 11:02:04.205000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (94, 46, 5, true, 0, 42.1, false, '2022-03-26 16:07:36.000000');
+VALUES (2, 40, 1, true, 0, 110, false, '2022-05-09 10:51:31.469000');
 INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (85, 40, 4, true, 0, 87.12, false, '2022-03-26 16:06:00.000000');
-INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (70, 46, 1, false, 0, 47.56, false, '2022-03-26 15:58:52.000000');
-INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (20, 40, 15, false, 0, 85.21, false, '2022-04-30 20:11:11.922780');
-INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (84, 40, 3, true, 0, 80.32, false, '2022-03-26 16:05:59.000000');
-INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (92, 46, 3, true, 0, 25.32, false, '2022-03-26 16:07:33.000000');
-INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (76, 34, 4, true, 0, 230.07, false, '2022-03-26 16:04:22.000000');
-INSERT INTO public.user_queue (user_queue_id, queue_id, user_id, is_active, skips, expenses, is_important, date_joined)
-VALUES (91, 46, 2, true, 0, 35.31, false, '2022-03-26 16:07:32.000000');
+VALUES (22, 46, 4, true, 0, 58.09, false, '2022-05-09 11:06:24.098000');
--- src/main/kotlin/com/innopolis/innoqueue/service/UserService.kt
+++ src/main/kotlin/com/innopolis/innoqueue/service/UserService.kt
@@ -27,17 +27,21 @@ class UserService(
 
     fun generateUserToken(userName: String, fcmToken: String): TokenDTO {
         // TODO remove after demo presentation
-        if (userName == "admin") {
-            val u = userRepository.findByIdOrNull(1)!!
-            u.fcmToken = fcmToken
-            userRepository.save(u)
-            return TokenDTO("11111", 1)
-        }
-        if (userName == "Emil") {
-            val u = userRepository.findByIdOrNull(15)!!
-            u.fcmToken = fcmToken
-            userRepository.save(u)
-            return TokenDTO("22222", 15)
+        val reservedUsers: List<Triple<String, Long, String>> = listOf(
+            Triple("admin", 1, "11111"),
+            Triple("Emil", 2, "22222"),
+            Triple("Roman", 3, "33333"),
+            Triple("Timur", 4, "44444"),
+            Triple("Ivan", 5, "55555")
+        )
+        for (r in reservedUsers) {
+            val (n, i, t) = r
+            if (userName == n) {
+                val u = userRepository.findByIdOrNull(i)!!
+                u.fcmToken = fcmToken
+                userRepository.save(u)
+                return TokenDTO(t, i)
+            }
         }
 
         if (userName.isEmpty()) {
