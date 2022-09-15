060e49d42a5145b2133699d2947f040dcac8e45d
Daniil Livitin <danya_livitin@mail.ru>
2022-09-15 22:05:13 +0300

    Add github action to build project after PR #24

--- src/test/kotlin/com/innopolis/innoqueue/InnoQueueApplicationTests.kt
+++ src/test/kotlin/com/innopolis/innoqueue/InnoQueueApplicationTests.kt
@@ -1,13 +1,7 @@
 package com.innopolis.innoqueue
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class InnoQueueApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
 }
