1dbb387d400eaba91d53463172d9b9bbe9cce64c
SMore-Napi <s.roma245@gmail.com>
2022-09-25 21:54:05 +0300

    Fix admin authorisation availability closes #36

--- src/main/kotlin/com/innopolis/innoqueue/service/UserService.kt
+++ src/main/kotlin/com/innopolis/innoqueue/service/UserService.kt
@@ -26,24 +26,6 @@ class UserService(
     }
 
     fun generateUserToken(userName: String, fcmToken: String): TokenDTO {
-        // TODO remove after demo presentation
-        val reservedUsers: List<Triple<String, Long, String>> = listOf(
-            Triple("admin", 1, "11111"),
-//            Triple("Emil", 2, "22222"),
-//            Triple("Roman", 3, "33333"),
-//            Triple("Timur", 4, "44444"),
-//            Triple("Ivan", 5, "55555")
-        )
-        for (r in reservedUsers) {
-            val (n, i, t) = r
-            if (userName == n) {
-                val u = userRepository.findByIdOrNull(i)!!
-                u.fcmToken = fcmToken
-                userRepository.save(u)
-                return TokenDTO(t, i)
-            }
-        }
-
         if (userName.isEmpty()) {
             throw IllegalArgumentException("Username can't be an empty string")
         }
