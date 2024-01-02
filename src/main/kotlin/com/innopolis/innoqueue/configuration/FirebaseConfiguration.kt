ad0d1460c87de47623341b5d02755464b34768e0
Roman Soldatov <49106163+SMore-Napi@users.noreply.github.com>
2024-01-02 16:55:00 +0400

    Refactor firebase configuration closes #83

--- src/main/kotlin/com/innopolis/innoqueue/configuration/FirebaseConfiguration.kt
+++ src/main/kotlin/com/innopolis/innoqueue/configuration/FirebaseConfiguration.kt
@@ -3,32 +3,33 @@ package com.innopolis.innoqueue.configuration
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
 import org.json.JSONObject
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.ByteArrayInputStream
-import java.io.IOException
 import java.io.InputStream
+
+private const val FIREBASE_APP_NAME = "InnoQueue"
 
 /**
  * Bean configuration for the Firebase options
  */
 @Configuration
-//@ConditionalOnProperty(name = ["firebase"], matchIfMissing = true)
 class FirebaseConfiguration {
     @Value("\${firebase}")
-    private val gservicesConfig: String? = null
+    private val googleServicesConfig: String? = null
 
     /**
-     * Bean which injects Firebase options
+     * Bean which injects Firebase options if GOOGLE_CREDENTIALS property was provided in the application.yml
      */
     @Bean
-    @Throws(IOException::class)
-    fun provideFirebaseOptions(): FirebaseApp? = if (gservicesConfig == "null") {
-        null
-    } else {
-        val jsonObject = JSONObject(gservicesConfig.toString())
+    @ConditionalOnProperty(name = ["firebase"])
+    fun firebaseApp(): FirebaseApp {
+        val jsonObject = JSONObject(googleServicesConfig.toString())
         val inputStream: InputStream = ByteArrayInputStream(jsonObject.toString().toByteArray())
         val googleCredentials = GoogleCredentials
             .fromStream(inputStream)
@@ -36,6 +37,16 @@ class FirebaseConfiguration {
             .builder()
             .setCredentials(googleCredentials)
             .build()
-        FirebaseApp.initializeApp(firebaseOptions, "InnoQueue")
+        return FirebaseApp.initializeApp(firebaseOptions, FIREBASE_APP_NAME)
+    }
+
+    /**
+     * Bean which injects FirebaseMessaging for sending push notifications.
+     * It will be created only if GOOGLE_CREDENTIALS property was provided in the application.yml
+     */
+    @Bean
+    @ConditionalOnBean(FirebaseApp::class)
+    fun firebaseMessaging(firebaseApp: FirebaseApp): FirebaseMessaging {
+        return FirebaseMessaging.getInstance(firebaseApp)
     }
 }
--- src/main/kotlin/com/innopolis/innoqueue/webclients/firebase/service/impl/FirebaseMessagingClientImpl.kt
+++ src/main/kotlin/com/innopolis/innoqueue/webclients/firebase/service/impl/FirebaseMessagingClientImpl.kt
@@ -1,6 +1,5 @@
 package com.innopolis.innoqueue.webclients.firebase.service.impl
 
-import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingException
 import com.google.firebase.messaging.Message
@@ -9,21 +8,21 @@ import com.innopolis.innoqueue.webclients.firebase.model.FirebaseMessage
 import com.innopolis.innoqueue.webclients.firebase.model.TitleBody
 import com.innopolis.innoqueue.webclients.firebase.service.FirebaseMessagingClient
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 
+@Primary
 @Service
-//@ConditionalOnBean(name = ["firebaseApp"])
+@ConditionalOnBean(FirebaseMessaging::class)
 class FirebaseMessagingClientImpl(
-    firebaseApp: FirebaseApp?
+    val firebaseMessaging: FirebaseMessaging
 ) : FirebaseMessagingClient {
     private val logger = LoggerFactory.getLogger(javaClass)
-
-    private val firebaseMessaging: FirebaseMessaging? = firebaseApp?.let { FirebaseMessaging.getInstance(it) }
-
     override fun sendFirebaseMessage(firebaseMessage: FirebaseMessage) {
         try {
             val message = firebaseMessage.message()
-            val response = firebaseMessaging?.send(message)
+            val response = firebaseMessaging.send(message)
             logger.info("Firebase response: $response")
         } catch (e: FirebaseMessagingException) {
             logger.error("Firebase exception: $e")
